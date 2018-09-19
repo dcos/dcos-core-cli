3bacff3be4d614e9dd852255396e8e179d19702e
Brian Lewis <keller@csh.rit.edu>
2018-09-19 16:14:41 +0200

    fix issues to hopefully build on windows

--- python/bin/env.sh
+++ python/bin/env.sh
@@ -14,8 +14,9 @@ if [ ! -d "${BUILDDIR}/${VENV}" ]; then
     PYTHON_MAJOR=$(${PYTHON} -c 'import sys; print(sys.version_info[0])')
     PYTHON_MINOR=$(${PYTHON} -c 'import sys; print(sys.version_info[1])')
 
-    if ([ "${PYTHON_MAJOR}" != "3" ] || [ "${PYTHON_MINOR}" != "5" ]) && ! [ -v DCOS_EXPERIMENTAL ]; then
-        echo "Cannot find supported python version 3.5+. Exiting..."
+    : "${DCOS_EXPERIMENTAL:=""}"
+    if [ "${PYTHON_MAJOR}" != "3" ] || ([ "${PYTHON_MINOR}" != "5" ] && [ "$DCOS_EXPERIMENTAL" != "" ]); then
+        echo "Cannot find supported python version 3.5. Exiting..."
         exit 1
     fi
     if [ "$(uname)" = "Windows_NT" ]; then
