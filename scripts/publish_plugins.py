bdcf02599cd72b05500a85605f0d5fabcbdf1c6b
Brian Lewis <keller@csh.rit.edu>
2018-09-21 12:14:49 +0200

    Refactor packaging scripts to create local plugin
    This refactors our plugin publishing flow to allow for the plugin to be
    created locally instead of being published to S3 immediately.
    It also will allow for completions to be packaged, though this is
    currently commented out as we have no completions to add yet.

--- scripts/publish_plugins.py
+++ scripts/publish_plugins.py
@@ -1,46 +1,10 @@
 #!/usr/bin/env python3
 
-import os
-import shutil
-
 import boto3
 
+from plugin import package_plugin
+
 version = os.environ.get("TAG_NAME", '1.12')
-
-plugin_toml = '''
-schema_version = 1
-name = "dcos-core-cli"
-
-[[commands]]
-name = "job"
-path = "bin/dcos{0}"
-description = "Deploy and manage jobs in DC/OS"
-
-[[commands]]
-name = "marathon"
-path = "bin/dcos{0}"
-description = "Deploy and manage applications to DC/OS"
-
-[[commands]]
-name = "node"
-path = "bin/dcos{0}"
-description = "View DC/OS node information"
-
-[[commands]]
-name = "package"
-path = "bin/dcos{0}"
-description = "Install and manage DC/OS software packages"
-
-[[commands]]
-name = "service"
-path = "bin/dcos{0}"
-description = "Manage DC/OS services"
-
-[[commands]]
-name = "task"
-path = "bin/dcos{0}"
-description = "Manage DC/OS tasks"
-'''
 
 build_path = os.path.dirname(os.path.realpath(__file__)) + "/../build"
 
@@ -48,16 +12,9 @@ platforms = ['linux', 'darwin', 'windows']
 
 for platform in platforms:
     plugin_path = build_path + '/' + platform + '/plugin'
-    bin_extension = '.exe' if platform == 'windows' else ''
 
-    with open(plugin_path + '/plugin.toml', encoding='utf-8', mode='w') as file:
-        file.write(plugin_toml.format(bin_extension))
+    package_plugin(plugin_path, platform)
 
-    shutil.make_archive(
-        '{}/{}/dcos-core-cli'.format(build_path, platform),
-        'zip',
-        plugin_path
-    )
 
 s3_client = boto3.resource('s3', region_name='us-west-2').meta.client
 
