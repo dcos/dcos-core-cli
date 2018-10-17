05ac6a4b7339c402c56b82a2b386cc9d7b57023d
Armand Grillet <ArmandGrillet@users.noreply.github.com>
2018-10-17 13:01:28 +0200

    Update python/lib/dcoscli/dcoscli/service/main.py

--- python/lib/dcoscli/dcoscli/service/main.py
+++ python/lib/dcoscli/dcoscli/service/main.py
@@ -6,7 +6,7 @@ from dcos import cmds, emitting, marathon, mesos, ssh_util, subprocess, util
 from dcos.errors import DCOSException, DefaultError
 from dcoscli import log, tables
 from dcoscli.subcommand import default_command_info, default_doc
-from dcoscli.util import decorate_docopt_usage, cluster_version_check
+from dcoscli.util import cluster_version_check, decorate_docopt_usage
 
 from ..task import main as task_main
 
