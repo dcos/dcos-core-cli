ec706bd37c88b4776e1a63294cace15cdef70232
Robin Gögge <r.goegge@outlook.com>
2019-01-11 09:10:07 +0100

    Updated help menu to follow UX guidelines.

--- pkg/cmd/dcos.go
+++ pkg/cmd/dcos.go
@@ -6,6 +6,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const annotationUsageOptions string = "usage_options"
+
 // NewDCOSCommand creates the `dcos` command with all the available subcommands.
 func NewDCOSCommand(ctx api.Context) *cobra.Command {
 	cmd := &cobra.Command{
@@ -18,6 +20,24 @@ func NewDCOSCommand(ctx api.Context) *cobra.Command {
 	cmd.AddCommand(
 		job.NewCommand(ctx),
 	)
+
+	// This follows the CLI design guidelines for help formatting.
+	cmd.SetUsageTemplate(`Usage:{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{.Name}}
+      {{.Short}}{{end}}{{end}}{{end}}{{if or .HasAvailableLocalFlags (ne (index .Annotations "` + annotationUsageOptions + `") "")}}
+
+Options:{{if ne (index .Annotations "` + annotationUsageOptions + `") ""}}{{index .Annotations "` + annotationUsageOptions + `"}}{{else}}
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`)
 
 	return cmd
 }
