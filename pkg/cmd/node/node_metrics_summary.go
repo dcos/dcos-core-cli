9ce1826f1960458d311a0eb99289631fbccebb22
Armand Grillet <armand.grillet@outlook.com>
2019-02-19 15:06:03 +0100

    Fixed node_metrics_summary.go.

--- pkg/cmd/node/node_metrics_summary.go
+++ pkg/cmd/node/node_metrics_summary.go
@@ -20,7 +20,7 @@ func newCmdNodeMetricsSummary(ctx api.Context) *cobra.Command {
 		Short: "Print summary of the metrics of an agent",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			node, err := metrics.NewClient(pluginutil.HTTPClient("")).Node(args[1])
+			node, err := metrics.NewClient(pluginutil.HTTPClient("")).Node(args[0])
 			if err != nil {
 				return err
 			}
