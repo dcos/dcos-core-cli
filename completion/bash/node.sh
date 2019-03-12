3d54e8334a073730946ddbbbdf91cf48c313d91f
Robin Gögge <r.goegge@outlook.com>
2019-03-12 16:32:22 +0100

    Adjust completion scripts for refactoring of 'dcos node'

--- completion/bash/node.sh
+++ completion/bash/node.sh
@@ -8,16 +8,15 @@ _dcos_node() {
 
     local flags=(
     "--help"
-    "--info"
     "--json"
     "--field="
-    "--version"
     )
 
     local commands=(
     "decommision"
     "diagnostics"
     "dns"
+    "list"
     "list-components"
     "log"
     "metrics"
@@ -51,16 +50,16 @@ _dcos_node_diagnostics() {
     fi
 
     local flags=(
-    "--list"
-    "--status"
-    "--cancel"
     "--json"
     )
 
     local commands=(
+    "cancel"
     "create"
     "delete"
     "download"
+    "list"
+    "status"
     )
 
     if [ -z "$command" ]; then
@@ -76,6 +75,10 @@ _dcos_node_diagnostics() {
     fi
 
     __dcos_handle_subcommand
+}
+
+_dcos_node_diagnostics_cancel() {
+    return
 }
 
 _dcos_node_diagnostics_create() {
@@ -109,6 +112,14 @@ _dcos_node_diagnostics_download() {
     fi
 }
 
+_dcos_node_diagnostics_list() {
+    return
+}
+
+_dcos_node_diagnostics_status() {
+    return
+}
+
 _dcos_node_dns() {
     local i command
 
@@ -126,6 +137,31 @@ _dcos_node_dns() {
                 __dcos_handle_compreply "${flags[@]}"
                 ;;
             *)
+                ;;
+        esac
+        return
+    fi
+}
+
+_dcos_node_list() {
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local flags=(
+    "--help"
+    "--json"
+    "--field="
+    )
+
+    if [ -z "$command" ]; then
+        case "$cur" in
+            --*)
+                __dcos_handle_compreply "${flags[@]}"
+                ;;
+            --)
                 ;;
         esac
         return
