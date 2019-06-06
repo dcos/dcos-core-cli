9c7ba6e85afd8c98ba075d36d5a5d414c1b18c01
Bilal Amarni <bilal.amarni@gmail.com>
2019-06-06 16:06:10 +0200

    Fix unknown subcommand error for `dcos service`
    Currently the service subcommand prints the following in case of an
    invalid subcommand:
    
    $ dcos service qwerty
    Getting the list of services from `dcos service` is deprecated. Please
    use `dcos service list`.
    Error: unknown command "qwerty" for "list"
    
    This updates it to print the help menu, an error message, and return
    with a non-zero exit code as well.
    
    https://jira.mesosphere.com/browse/DCOS_OSS-3500

--- pkg/cmd/service/service.go
+++ pkg/cmd/service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/dcos/dcos-cli/api"
 	"github.com/dcos/dcos-cli/pkg/cli"
 	"github.com/spf13/cobra"
@@ -12,18 +14,22 @@ func NewCommand(ctx api.Context) *cobra.Command {
 		Use:   "service",
 		Short: "Manage DC/OS services",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			_, ok := ctx.EnvLookup(cli.EnvStrictDeprecations)
-			if !ok {
-				ctx.Deprecated("Getting the list of services from `dcos service` is deprecated. Please use `dcos service list`.")
-				listCmd := newCmdServiceList(ctx)
-				// Execute by default would use os.Args[1:], which is everything after `dcos ...`.
-				// We need all command line arguments after `dcos service ...`.
-				listCmd.SetArgs(ctx.Args()[2:])
-				listCmd.SilenceErrors = true
-				listCmd.SilenceUsage = true
-				return listCmd.Execute()
+			if len(args) == 0 {
+				_, ok := ctx.EnvLookup(cli.EnvStrictDeprecations)
+				if !ok {
+					ctx.Deprecated("Getting the list of services from `dcos service` is deprecated. Please use `dcos service list`.")
+					listCmd := newCmdServiceList(ctx)
+					// Execute by default would use os.Args[1:], which is everything after `dcos ...`.
+					// We need all command line arguments after `dcos service ...`.
+					listCmd.SetArgs(ctx.Args()[2:])
+					listCmd.SilenceErrors = true
+					listCmd.SilenceUsage = true
+					return listCmd.Execute()
+				}
+				return cmd.Help()
 			}
-			return cmd.Help()
+			fmt.Fprintln(ctx.ErrOut(), cmd.UsageString())
+			return fmt.Errorf("unknown command %s", args[0])
 		},
 	}
 	cmd.Flags().Bool("json", false, "Print in json format")
