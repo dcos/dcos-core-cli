403dce08703727f3ba4a38af4375b0b8f712f706
Bilal Amarni <bilal.amarni@gmail.com>
2019-06-26 16:16:37 +0200

    Added task metrics details.

--- pkg/cmd/task/task_metrics_details.go
+++ pkg/cmd/task/task_metrics_details.go
@@ -1,7 +1,13 @@
 package task
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/dcos/dcos-cli/api"
+	"github.com/dcos/dcos-cli/pkg/cli"
+	"github.com/dcos/dcos-core-cli/pkg/metrics"
+	"github.com/dcos/dcos-core-cli/pkg/pluginutil"
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +19,52 @@ func newCmdTaskMetricsDetails(ctx api.Context) *cobra.Command {
 		Short: "Print a table of all the metrics for a given task",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			task, err := findTask(ctx, args[0])
+			if err != nil {
+				return err
+			}
+			status := task.Statuses[0]
+			containerID := status.ContainerStatus.GetContainerID().Value
+
+			c := metrics.NewClient(pluginutil.HTTPClient(""))
+			taskMetrics, err := c.Task(task.AgentID.Value, containerID)
+			if err != nil {
+				return err
+			}
+			appMetrics, err := c.App(task.AgentID.Value, containerID)
+			if err != nil {
+				return err
+			}
+
+			if taskMetrics == nil && appMetrics == nil {
+				return fmt.Errorf("No metrics found for task '%s'", task.TaskID.Value)
+			}
+			datapoints := []metrics.Datapoint{}
+			if taskMetrics != nil {
+				datapoints = append(datapoints, taskMetrics.Datapoints...)
+			}
+
+			if appMetrics != nil {
+				datapoints = append(datapoints, appMetrics.Datapoints...)
+			}
+
+			if jsonOutput {
+				enc := json.NewEncoder(ctx.Out())
+				enc.SetIndent("", "    ")
+				return enc.Encode(datapoints)
+			}
+
+			table := cli.NewTable(ctx.Out(), []string{"NAME", "VALUE"})
+			for _, datapoint := range datapoints {
+				value := ""
+				if datapoint.Value == float64(int(datapoint.Value)) {
+					value = fmt.Sprintf("%.0f", datapoint.Value)
+				} else {
+					value = fmt.Sprintf("%.2f", datapoint.Value)
+				}
+				table.Append([]string{datapoint.Name, value})
+			}
+			table.Render()
 			return nil
 		},
 	}
--- pkg/metrics/types.go
+++ pkg/metrics/types.go
@@ -12,6 +12,26 @@ type Node struct {
 	} `json:"dimensions"`
 }
 
+// Container represents the metrics of a container.
+type Container struct {
+	Datapoints []Datapoint `json:"datapoints"`
+	Dimensions struct {
+		MesosID       string `json:"mesos_id"`
+		ClusterID     string `json:"cluster_id"`
+		ContainerID   string `json:"container_id"`
+		FrameworkName string `json:"framework_name"`
+		TaskName      string `json:"task_name"`
+		Hostname      string `json:"hostname"`
+		Labels        struct {
+			DcosClusterID     string `json:"dcos_cluster_id"`
+			DcosClusterName   string `json:"dcos_cluster_name"`
+			FaultDomainRegion string `json:"fault_domain_region"`
+			FaultDomainZone   string `json:"fault_domain_zone"`
+			Host              string `json:"host"`
+		} `json:"labels"`
+	} `json:"dimensions"`
+}
+
 // Datapoint represents a datapoint of a node.
 type Datapoint struct {
 	Name      string            `json:"name"`
