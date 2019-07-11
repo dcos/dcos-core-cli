c4ca0798cc9c495bfa38b8e88497d6ec0f49061e
Armand Grillet <armand.grillet@outlook.com>
2019-07-11 10:37:01 +0200

    Revert "WIP back to good CI but with changes on client."
    This reverts commit 52b68f0b4f839067b319bc688a2f7ce5a6012b7a.

--- pkg/cmd/node/node_ssh.go
+++ pkg/cmd/node/node_ssh.go
@@ -37,15 +37,7 @@ func newCmdNodeSSH(ctx api.Context) *cobra.Command {
 				return err
 			}
 
-			strictHostKeyChecking := true
-			for _, sshOption := range clientOpts.SSHOptions {
-				if sshOption == "StrictHostKeyChecking=no" {
-					strictHostKeyChecking = false
-				}
-			}
-
-			if strictHostKeyChecking && masterProxy {
-				fmt.Println("YOLO")
+			if masterProxy {
 				initialClientOpts := sshclient.ClientOpts{
 					Input:  ctx.Input(),
 					Out:    ctx.Out(),
@@ -54,6 +46,11 @@ func newCmdNodeSSH(ctx api.Context) *cobra.Command {
 
 				initialClientOpts.User = clientOpts.User
 				initialClientOpts.Host = clientOpts.Proxy
+				for _, sshOption := range clientOpts.SSHOptions {
+					if sshOption == "StrictHostKeyChecking=no" {
+						initialClientOpts.SSHOptions = []string{"StrictHostKeyChecking=no"}
+					}
+				}
 				initialSSHClient, err := sshclient.NewClient(initialClientOpts, pluginutil.Logger())
 				if err != nil {
 					return err
