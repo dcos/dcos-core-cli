965d753cf5d3042c603b27bd13b775cb00b51510
Armand Grillet <armand.grillet@outlook.com>
2019-07-31 11:16:09 +0200

    Improved 'dcos quota list'.

--- pkg/cmd/quota/quota_list.go
+++ pkg/cmd/quota/quota_list.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/dcos/dcos-cli/api"
+	"github.com/dcos/dcos-cli/pkg/cli"
 	"github.com/dcos/dcos-core-cli/pkg/marathon"
 	"github.com/dcos/dcos-core-cli/pkg/mesos"
 	"github.com/spf13/cobra"
@@ -64,9 +65,59 @@ func newCmdQuotaList(ctx api.Context) *cobra.Command {
 				return enc.Encode(quotas)
 			}
 
+			tableHeader := []string{"NAME", "CPU CONSUMED", "MEMORY CONSUMED", "DISK CONSUMED", "GPU CONSUMED"}
+			table := cli.NewTable(ctx.Out(), tableHeader)
+
 			for _, quota := range quotas {
-				fmt.Fprintln(ctx.Out(), quota.Role)
+				quotaInfo := map[string]string{"cpus": "", "mem": "", "disk": "", "gpus": ""}
+
+				for info := range quotaInfo {
+					if limit, ok := quota.Limit[info].(float64); ok {
+						var cons float64
+						if consumed, ok := quota.Consumed[info].(float64); ok {
+							cons = consumed
+						}
+						percent := fmt.Sprintf("%.2f%%", cons*100/limit)
+						var desc string
+						switch info {
+						case "cpus", "gpus":
+							desc = fmt.Sprintf("%.0f of %.0f Cores", cons, limit)
+						case "mem", "disk":
+							if limit < 1000 {
+								desc = fmt.Sprintf("%.0f MiB of %.0f MiB", cons, limit)
+							} else {
+								cons /= 1000
+								consString := fmt.Sprintf("%.1f", cons)
+								if cons == float64(int64(cons)) {
+									consString = fmt.Sprintf("%.0f", cons)
+								}
+
+								limit /= 1000
+								limitString := fmt.Sprintf("%.1f", limit)
+								if limit == float64(int64(limit)) {
+									limitString = fmt.Sprintf("%.0f", limit)
+								}
+
+								desc = fmt.Sprintf("%s GiB of %s GiB", consString, limitString)
+							}
+						}
+						quotaInfo[info] = fmt.Sprintf("%s (%s)", percent, desc)
+					} else {
+						quotaInfo[info] = "No limit"
+					}
+				}
+
+				tableItem := []string{
+					quota.Role,
+					quotaInfo["cpus"],
+					quotaInfo["mem"],
+					quotaInfo["disk"],
+					quotaInfo["gpus"],
+				}
+				table.Append(tableItem)
 			}
+
+			table.Render()
 			return nil
 		},
 	}
