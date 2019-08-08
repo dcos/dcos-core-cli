a4b1577505cc5cfa17b03ef9ef8c01a0a80591c5
Robin Gögge <r.goegge@outlook.com>
2019-08-08 13:59:10 +0200

    Improve error messages on metronome API errors

--- pkg/metronome/error.go
+++ pkg/metronome/error.go
@@ -4,8 +4,14 @@ import "strconv"
 
 // Error is a standard error returned by the DC/OS API.
 type Error struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    int      `json:"code"`
+	Message string   `json:"message"`
+	Details []detail `json:"details"`
+}
+
+type detail struct {
+	Path   string   `json:"path"`
+	Errors []string `json:"errors"`
 }
 
 // Error converts an API error to a string.
