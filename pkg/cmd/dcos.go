52cee67b125fc5f8cd3f19bd704f980c55aa1b1c
Robin Gögge <r.goegge@outlook.com>
2019-08-13 15:57:23 +0200

    Fix help output to match UX styleguide
    https://jira.mesosphere.com/browse/DCOS_OSS-5110

--- pkg/cmd/dcos.go
+++ pkg/cmd/dcos.go
@@ -9,6 +9,7 @@ import (
 	"github.com/dcos/dcos-core-cli/pkg/cmd/quota"
 	"github.com/dcos/dcos-core-cli/pkg/cmd/service"
 	"github.com/dcos/dcos-core-cli/pkg/cmd/task"
+	"github.com/dcos/dcos-core-cli/pkg/pluginutil"
 	"github.com/spf13/cobra"
 )
 
@@ -18,10 +19,6 @@ const annotationUsageOptions string = "usage_options"
 func NewDCOSCommand(ctx api.Context) *cobra.Command {
 	cmd := &cobra.Command{
 		Use: "dcos",
-		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			cmd.SilenceUsage = true
-			cmd.SilenceErrors = true
-		},
 	}
 
 	cmd.AddCommand(
@@ -34,23 +31,9 @@ func NewDCOSCommand(ctx api.Context) *cobra.Command {
 		diagnostics.NewCommand(ctx),
 	)
 
-	// This follows the CLI design guidelines for help formatting.
-	cmd.SetUsageTemplate(`Usage:{{if .HasAvailableSubCommands}}
-  {{.CommandPath}} [command]{{else if .Runnable}}
-  {{.UseLine}}{{end}}{{if .HasExample}}
-
-Examples:
-{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
-
-Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
-  {{.Name}}
-      {{.Short}}{{end}}{{end}}{{end}}{{if or .HasAvailableLocalFlags (ne (index .Annotations "` + annotationUsageOptions + `") "")}}
-
-Options:{{if ne (index .Annotations "` + annotationUsageOptions + `") ""}}{{index .Annotations "` + annotationUsageOptions + `"}}{{else}}
-{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
-
-Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
-`)
+	cmd.SetUsageFunc(pluginutil.Usage)
+	cmd.SilenceUsage = true
+	cmd.SilenceErrors = true
 
 	return cmd
 }
--- pkg/pluginutil/pluginutil.go
+++ pkg/pluginutil/pluginutil.go
@@ -3,15 +3,19 @@ package pluginutil
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
+	"text/template"
 
 	"github.com/dcos/client-go/dcos"
 	"github.com/dcos/dcos-cli/pkg/httpclient"
 	"github.com/dcos/dcos-cli/pkg/log"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 // HTTPClient returns an HTTP client from a plugin runtime.
@@ -105,4 +109,46 @@ func Logger() *logrus.Logger {
 	}
 
 	return logger
+}
+
+// Usage defines a help template folowing the UX guidelines
+func Usage(command *cobra.Command) error {
+	tpl := template.New("top")
+	template.Must(tpl.Parse(`Usage:{{if .HasAvailableSubCommands}}
+    {{.CommandPath}} [command]{{else if .Runnable}}
+    {{.UseLine}}{{end}}{{if .HasExample}}
+
+Examples:
+    {{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+    {{.Name}}
+        {{.Short}}{{end}}{{end}}{{end}}
+`))
+
+	err := tpl.Execute(os.Stdout, command)
+	if err != nil {
+		return err
+	}
+
+	if command.HasAvailableLocalFlags() {
+		fmt.Println("\nOptions:")
+		command.LocalFlags().VisitAll(func(f *pflag.Flag) {
+			if f.Hidden {
+				return
+			}
+			if f.Shorthand != "" && f.Name != "" {
+				fmt.Printf("    -%s, --%s\n", f.Shorthand, f.Name)
+			} else if f.Shorthand != "" {
+				fmt.Printf("    -%s\n", f.Shorthand)
+			} else {
+				fmt.Printf("    --%s\n", f.Name)
+			}
+			fmt.Println("        " + f.Usage)
+		})
+	}
+	if command.HasAvailableSubCommands() {
+		fmt.Println("\n" + `Use "` + command.CommandPath() + ` [command] --help" for more information about a command.`)
+	}
+	return nil
 }
