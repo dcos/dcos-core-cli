888378e49df7c5d6fc7c6ceca2e1e880850c1fe8
Brian Lewis <keller@csh.rit.edu>
2019-08-13 14:23:37 +0200

    Add diagnostics and diagnostics list commands

---
+++ pkg/cmd/diagnostics/diagnostics_list.go
@@ -0,0 +1,53 @@
+package diagnostics
+
+import (
+	"encoding/json"
+
+	"github.com/dcos/dcos-cli/api"
+	"github.com/dcos/dcos-cli/pkg/cli"
+	diagnostics "github.com/dcos/dcos-core-cli/pkg/diagnostics/v2"
+	"github.com/dcos/dcos-core-cli/pkg/pluginutil"
+	"github.com/dustin/go-humanize"
+	"github.com/spf13/cobra"
+)
+
+func newDiagnosticsListCommand(ctx api.Context) *cobra.Command {
+	var jsonOutput bool
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "List available bundles",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			c := pluginutil.HTTPClient("")
+			client := diagnostics.NewClient(c)
+			bundles, err := client.List()
+			if err != nil {
+				return err
+			}
+
+			if jsonOutput {
+				enc := json.NewEncoder(ctx.Out())
+				enc.SetIndent("", "    ")
+				return enc.Encode(bundles)
+			}
+
+			tableHeader := []string{"ID", "STATUS", "CREATED", "SIZE"}
+			table := cli.NewTable(ctx.Out(), tableHeader)
+
+			for _, b := range bundles {
+				size := humanize.Bytes(uint64(b.Size))
+				stopped := humanize.Time(b.Stopped)
+
+				fields := []string{b.ID, b.Status.String(), stopped, size}
+				table.Append(fields)
+			}
+
+			table.Render()
+
+			return nil
+		},
+	}
+
+	cmd.Flags().BoolVar(&jsonOutput, "json", false, "Print in json format")
+
+	return cmd
+}
