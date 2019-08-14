7e5b471ba52407857c6fa17ba203a73a3745c49b
Brian Lewis <keller@csh.rit.edu>
2019-08-14 17:18:50 +0200

    Add diagnostics download command

--- pkg/diagnostics/v2/client_test.go
+++ pkg/diagnostics/v2/client_test.go
@@ -2,8 +2,10 @@ package v2
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 	"time"
 
@@ -51,4 +53,85 @@ func TestEmptyList(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Empty(t, bundles)
+}
+
+func TestDownload(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/system/health/v1/diagnostics/bundle-0/file", r.URL.String())
+		assert.Equal(t, "GET", r.Method)
+
+		http.ServeFile(w, r, "testdata/bundle.zip")
+	}))
+	defer ts.Close()
+
+	file, err := ioutil.TempFile(os.TempDir(), "bundle-0.zip")
+	require.NoError(t, err)
+	defer os.Remove(file.Name())
+
+	c := NewClient(pluginutil.HTTPClient(ts.URL))
+	err = c.Download("bundle-0", file)
+	require.NoError(t, err)
+	defer file.Close()
+
+	outputData, err := ioutil.ReadFile(file.Name())
+	require.NoError(t, err)
+
+	testData, err := ioutil.ReadFile("testdata/bundle.zip")
+	require.NoError(t, err)
+
+	assert.EqualValues(t, testData, outputData)
+}
+
+func TestDownloadBundleErrors(t *testing.T) {
+	type testDef struct {
+		name       string
+		returnCode int
+		errMessage string
+	}
+
+	tests := []testDef{
+		{
+			name:       "not found",
+			returnCode: http.StatusNotFound,
+			errMessage: "no bundle bundle-0 found",
+		},
+		{
+			name:       "internal server error",
+			returnCode: http.StatusInternalServerError,
+			errMessage: "bundle bundle-0 not readable",
+		},
+		{
+			name:       "not modified",
+			returnCode: http.StatusNotModified,
+			errMessage: "bundle bundle-0 canceled or not completed",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "/system/health/v1/diagnostics/bundle-0/file", r.URL.String())
+				assert.Equal(t, "GET", r.Method)
+
+				w.WriteHeader(test.returnCode)
+			}))
+			defer ts.Close()
+
+			file, err := ioutil.TempFile(os.TempDir(), "bundle-0.zip")
+			require.NoError(t, err)
+			defer os.Remove(file.Name())
+
+			c := NewClient(pluginutil.HTTPClient(ts.URL))
+			err = c.Download("bundle-0", file)
+			assert.Error(t, err)
+			assert.EqualError(t, err, test.errMessage)
+
+			file.Close()
+
+			outputData, err := ioutil.ReadFile(file.Name())
+			require.NoError(t, err)
+
+			assert.Empty(t, outputData)
+		})
+	}
 }
