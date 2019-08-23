209af030e5bc5171c95ab0c31b53a5965f94ee3a
Brian Lewis <keller@csh.rit.edu>
2019-08-23 15:19:36 +0200

    Add completions for the other diagnostics commands

--- completion/bash/diagnostics.sh
+++ completion/bash/diagnostics.sh
@@ -11,6 +11,9 @@ _dcos_diagnostics() {
     )
 
     local commands=(
+    "create"
+    "delete"
+    "download"
     "list"
     )
 
@@ -27,6 +30,79 @@ _dcos_diagnostics() {
     fi
 
     __dcos_handle_subcommand
+}
+
+_dcos_diagnostics_create() {
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local flags=(
+    "--help"
+    )
+
+    if [ -z "$command" ]; then
+        case "$cur" in
+            --*)
+                __dcos_handle_compreply "${flags[@]}"
+                ;;
+        esac
+        return
+    fi
+}
+
+_dcos_diagnostics_delete() {
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local flags=(
+    "--help"
+    )
+
+    if [ -z "$command" ]; then
+        case "$cur" in
+            --*)
+                __dcos_handle_compreply "${flags[@]}"
+                ;;
+            *)
+                __dcos_complete_bundle_ids
+                ;;
+        esac
+        return
+    fi
+}
+
+_dcos_diagnostics_download() {
+    local i command
+
+    if ! __dcos_default_command_parse; then
+        return
+    fi
+
+    local flags=(
+    "--help"
+    "--output"
+    )
+
+    # the normal `if` here omitted to make the optional file completion work
+    case "$cur" in
+        --*)
+            __dcos_handle_compreply "${flags[@]}"
+            ;;
+        *)
+            if [ "$prev" == "--output" ]; then
+                # if the last word was output then we return which causes
+                # bash to use the default file completion
+                return
+            fi
+            __dcos_complete_bundle_ids
+            ;;
+    esac
 }
 
 _dcos_diagnostics_list() {
@@ -49,4 +125,9 @@ _dcos_diagnostics_list() {
         esac
         return
     fi
+}
+
+__dcos_complete_bundle_ids() {
+    while IFS=$'\n' read -r line; do bundle_ids+=("$line"); done < <(dcos diagnostics list -q 2> /dev/null)
+    __dcos_handle_compreply "${bundle_ids[@]}"
 }
