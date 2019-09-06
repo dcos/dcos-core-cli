b2301580db2ba91bc29567d98eb25bc2ba1e0ca3
Tomasz Janiszewski <janiszt@gmail.com>
2019-09-06 16:56:58 +0200

    Impement pkg describe in Go
    Fixes: https://jira.mesosphere.com/browse/DCOS_OSS-4773

--- pkg/cmd/pkg/package_describe.go
+++ pkg/cmd/pkg/package_describe.go
@@ -1,7 +1,13 @@
 package pkg
 
 import (
+	"encoding/base64"
+	"encoding/json"
+
+	"github.com/dcos/client-go/dcos"
 	"github.com/dcos/dcos-cli/api"
+	"github.com/dcos/dcos-core-cli/pkg/cosmos"
+	"github.com/dcos/dcos-core-cli/pkg/pluginutil"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +20,61 @@ func newCmdPackageDescribe(ctx api.Context) *cobra.Command {
 		Short: "Get specific details for packages",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return invokePythonCLI(ctx)
+			packageName := args[0]
+
+			c, err := cosmos.NewClient(ctx, pluginutil.HTTPClient(""))
+			if err != nil {
+				return err
+			}
+			enc := json.NewEncoder(ctx.Out())
+			enc.SetIndent("", "    ")
+
+			if allVersions {
+				versions, err := c.PackageListVersions(packageName)
+				if err != nil {
+					return err
+				}
+				return enc.Encode(versions)
+
+			}
+
+			description, err := c.PackageDescribe(packageName, version)
+			if err != nil {
+				return err
+			}
+
+			if appOnly {
+				template, err := base64.StdEncoding.DecodeString(description.Package.Marathon.V2AppMustacheTemplate)
+				if err != nil {
+					return err
+				}
+				_, err = ctx.Out().Write(template)
+				return err
+			}
+
+			if cliOnly {
+				if !isEmptyCli(description.Package.Resource.Cli) {
+					return enc.Encode(description.Package.Resource.Cli)
+				}
+				if !isEmptyCommand(description.Package.Command) {
+					return enc.Encode(description.Package.Command)
+				}
+				return nil
+			}
+
+			if render {
+				template, err := c.PackageRender(appID, packageName, version, optionsPath)
+				if err != nil {
+					return err
+				}
+				return enc.Encode(template)
+			}
+
+			if config {
+				return enc.Encode(description.Package.Config)
+			}
+
+			return enc.Encode(description)
 		},
 	}
 	cmd.Flags().BoolVar(&appOnly, "app", false, "Application only")
@@ -26,4 +86,19 @@ func newCmdPackageDescribe(ctx api.Context) *cobra.Command {
 	cmd.Flags().BoolVar(&allVersions, "package-versions", false, " Displays all versions for this package")
 	cmd.Flags().BoolVar(&render, "render", false, "Collate the `marathon.json` package template with values from the `config.json` and `--options`")
 	return cmd
+}
+
+func isEmptyCommand(cmd dcos.CosmosPackageCommand) bool {
+	return cmd.Name == "" && len(cmd.Pip) == 0
+}
+
+func isEmptyCli(cli dcos.CosmosPackageResourceCli) bool {
+	return isEmptyBinary(cli.Binaries.Darwin) &&
+		isEmptyBinary(cli.Binaries.Linux) &&
+		isEmptyBinary(cli.Binaries.Windows)
+}
+
+func isEmptyBinary(binary dcos.CosmosPackageResourceCliOsBinaries) bool {
+	x := binary.X8664
+	return len(x.ContentHash) == 0 && x.Kind == "" && x.Url == ""
 }
