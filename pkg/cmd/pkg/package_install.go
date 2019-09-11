4c9225b008a64a9ca89702da6db6ec63962dd0db
Tomasz Janiszewski <janiszt@gmail.com>
2019-09-11 17:41:40 +0200

    Remove deprecated global option
    https://github.com/dcos/dcos-core-cli/blob/109f5bf4da861f1740f13aa34791af091f2feb17/python/lib/dcoscli/dcoscli/package/main.py#L327-L329

--- pkg/cmd/pkg/package_install.go
+++ pkg/cmd/pkg/package_install.go
@@ -6,7 +6,7 @@ import (
 )
 
 func newCmdPackageInstall(ctx api.Context) *cobra.Command {
-	var appOnly, cliOnly, global, yes bool
+	var appOnly, cliOnly, yes bool
 	var appID, optionsPath, version string
 
 	cmd := &cobra.Command{
@@ -20,7 +20,6 @@ func newCmdPackageInstall(ctx api.Context) *cobra.Command {
 	cmd.Flags().BoolVar(&appOnly, "app", false, "Application only")
 	cmd.Flags().StringVar(&appID, "app-id", "", "The application ID")
 	cmd.Flags().BoolVar(&cliOnly, "cli", false, "Command line only")
-	cmd.Flags().BoolVar(&global, "global", false, "Install a subcommand for all configured clusters")
 	cmd.Flags().StringVar(&optionsPath, "options", "", "Path to a JSON file that contains customized package installation options")
 	cmd.Flags().StringVar(&version, "package-version", "", "The package version")
 	cmd.Flags().BoolVar(&yes, "yes", false, "Disable interactive mode and assume “yes” is the answer to all prompts")
