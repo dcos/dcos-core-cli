b26cc41025d97a05ef49515538c9225dc759b74f
Tomasz Janiszewski <janiszt@gmail.com>
2019-09-20 16:45:12 +0200

    Add tests for describe

--- pkg/cmd/pkg/package_describe.go
+++ pkg/cmd/pkg/package_describe.go
@@ -12,82 +12,89 @@ import (
 	"github.com/dcos/dcos-core-cli/pkg/pluginutil"
 )
 
-func newCmdPackageDescribe(ctx api.Context) *cobra.Command {
-	var allVersions, appOnly, cliOnly, config, render bool
-	var appID, optionsPath, version string
+type describeOptions struct {
+	appID       string
+	optionsPath string
+	version     string
 
+	allVersions bool
+	appOnly     bool
+	cliOnly     bool
+	config      bool
+	render      bool
+}
+
+func newCmdPackageDescribe(ctx api.Context) *cobra.Command {
+	var opts describeOptions
 	cmd := &cobra.Command{
 		Use:   "describe <package-name>",
 		Short: "Get specific details for packages",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			packageName := args[0]
-
-			c, err := cosmos.NewClient(ctx, pluginutil.HTTPClient(""))
-			if err != nil {
-				return err
-			}
-			enc := json.NewEncoder(ctx.Out())
-			enc.SetIndent("", "  ")
-
-			if allVersions {
-				versions, err := c.PackageListVersions(packageName)
-				if err != nil {
-					return err
-				}
-				return enc.Encode(versions)
-
-			}
-
-			description, err := c.PackageDescribe(packageName, version)
-			if err != nil {
-				return err
-			}
-
-			if appOnly {
-				// If the user supplied template options, they definitely want to render the template
-				if render || optionsPath != "" {
-					template, err := c.PackageRender(appID, packageName, version, optionsPath)
-					if err != nil {
-						return err
-					}
-					return enc.Encode(template)
-				}
-
-				template, err := base64.StdEncoding.DecodeString(description.Package.Marathon.V2AppMustacheTemplate)
-				if err != nil {
-					return err
-				}
-				_, err = ctx.Out().Write(template)
-				return err
-			}
-
-			if cliOnly {
-				if !isEmptyCli(description.Package.Resource.Cli) {
-					return enc.Encode(description.Package.Resource.Cli)
-				}
-				if !isEmptyCommand(description.Package.Command) {
-					return enc.Encode(description.Package.Command)
-				}
-				return nil
-			}
-
-			if config {
-				return enc.Encode(description.Package.Config)
-			}
-
-			return enc.Encode(description)
+			return pkgDescribe(ctx, args[0], opts)
 		},
 	}
-	cmd.Flags().BoolVar(&appOnly, "app", false, "Application only")
-	cmd.Flags().StringVar(&appID, "app-id", "", "The application ID")
-	cmd.Flags().BoolVar(&cliOnly, "cli", false, "Command line only")
-	cmd.Flags().BoolVar(&config, "config", false, "Print the configurable properties of the `marathon.json` file")
-	cmd.Flags().StringVar(&optionsPath, "options", "", "Path to a JSON file that contains customized package installation options")
-	cmd.Flags().StringVar(&version, "package-version", "", "The package version")
-	cmd.Flags().BoolVar(&allVersions, "package-versions", false, " Displays all versions for this package")
-	cmd.Flags().BoolVar(&render, "render", false, "Collate the `marathon.json` package template with values from the `config.json` and `--options`")
+	cmd.Flags().BoolVar(&opts.appOnly, "app", false, "Application only")
+	cmd.Flags().StringVar(&opts.appID, "app-id", "", "The application ID")
+	cmd.Flags().BoolVar(&opts.cliOnly, "cli", false, "Command line only")
+	cmd.Flags().BoolVar(&opts.config, "config", false, "Print the opts.configurable properties of the `marathon.json` file")
+	cmd.Flags().StringVar(&opts.optionsPath, "options", "", "Path to a JSON file that contains customized package installation describeOptions")
+	cmd.Flags().StringVar(&opts.version, "package-version", "", "The package opts.version")
+	cmd.Flags().BoolVar(&opts.allVersions, "package-versions", false, " Displays all versions for this package")
+	cmd.Flags().BoolVar(&opts.render, "render", false,
+		"Collate the `marathon.json` package template with values from the `opts.config.json` and `--describeOptions`")
 	return cmd
+}
+
+func pkgDescribe(ctx api.Context, packageName string, opts describeOptions) error {
+	c, err := cosmos.NewClient(ctx, pluginutil.HTTPClient(""))
+	if err != nil {
+		return err
+	}
+	enc := json.NewEncoder(ctx.Out())
+	enc.SetIndent("", "  ")
+	if opts.allVersions {
+		versions, err := c.PackageListVersions(packageName)
+		if err != nil {
+			return err
+		}
+		return enc.Encode(versions)
+
+	}
+	description, err := c.PackageDescribe(packageName, opts.version)
+	if err != nil {
+		return err
+	}
+	if opts.appOnly {
+		// If the user supplied template describeOptions, they definitely want to render the template
+		if opts.render || opts.optionsPath != "" {
+			template, err := c.PackageRender(opts.appID, packageName, opts.version, opts.optionsPath)
+			if err != nil {
+				return err
+			}
+			return enc.Encode(template)
+		}
+
+		template, err := base64.StdEncoding.DecodeString(description.Package.Marathon.V2AppMustacheTemplate)
+		if err != nil {
+			return err
+		}
+		_, err = ctx.Out().Write(template)
+		return err
+	}
+	if opts.cliOnly {
+		if !isEmptyCli(description.Package.Resource.Cli) {
+			return enc.Encode(description.Package.Resource.Cli)
+		}
+		if !isEmptyCommand(description.Package.Command) {
+			return enc.Encode(description.Package.Command)
+		}
+		return nil
+	}
+	if opts.config {
+		return enc.Encode(description.Package.Config)
+	}
+	return enc.Encode(description)
 }
 
 func isEmptyCommand(cmd dcos.CosmosPackageCommand) bool {
