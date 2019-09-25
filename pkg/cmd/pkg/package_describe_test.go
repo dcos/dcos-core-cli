42fd032c0c20fc432d6299041993455641724fbc
Tomasz Janiszewski <janiszt@gmail.com>
2019-09-25 17:07:38 +0200

    Use minimal example data

--- pkg/cmd/pkg/package_describe_test.go
+++ pkg/cmd/pkg/package_describe_test.go
@@ -2,7 +2,8 @@ package pkg
 
 import (
 	"bytes"
-	"encoding/json"
+	"fmt"
+	"github.com/dcos/client-go/dcos"
 	"github.com/dcos/dcos-core-cli/pkg/cosmos"
 	"github.com/dcos/dcos-core-cli/pkg/cosmos/mocks"
 	"io/ioutil"
@@ -12,7 +13,6 @@ import (
 	"github.com/dcos/dcos-cli/pkg/mock"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func Test_pkgDescribe(t *testing.T) {
@@ -26,40 +26,180 @@ func Test_pkgDescribe(t *testing.T) {
 	ctx := mock.NewContext(env)
 
 	client := &mocks.Client{}
-	path := filepath.Join("testdata", "cosmos_chronos_describe_response.json")
-	raw, err := ioutil.ReadFile(path)
-	require.NoError(t, err)
-	var desc cosmos.Description
-	err = json.Unmarshal(raw, &desc)
-	require.NoError(t, err)
+	desc := cosmos.Description{
+		Package: dcos.CosmosPackage{
+			Description:      "Package stub with only required fields",
+			Maintainer:       "John Doe",
+			Name:             "helloworld",
+			PackagingVersion: "0.0.1",
+			ReleaseVersion:   9223372036854775807,
+			Version:          "0.0.2",
+			Marathon: dcos.CosmosPackageMarathon{
+				V2AppMustacheTemplate:
+				"ewogICAgImlkIjogInt7aGVsbG93b3JsZC5pZH19IiwKICAgICJjbWQiOiAiZWNobyAnSGVsbG8gV29ybGQnIgp9",
+			},
+			Config: map[string]interface{}{
+				"foo": "bar",
+			},
+		},
+	}
 	client.On("PackageDescribe", "helloworld", "").Return(&desc, nil)
 	client.On("PackageListVersions", "helloworld").Return([]string{"1", "2", "3"}, nil)
 	client.On("PackageRender", "", "helloworld", "", "").
 		Return(map[string]interface{}{"foo": "bar"}, nil)
 
+	path := filepath.Join("testdata", "describe.expected.json")
+	expectedDescribe, err := ioutil.ReadFile(path)
+	assert.NoError(t, err)
+
 	var testCases = []struct {
 		in  describeOptions
 		out string
 	}{
-		{describeOptions{}, "cosmos_chronos_describe.expected.json"},
-		{describeOptions{cliOnly: true}, "cosmos_chronos_describe_cli.expected.txt"},
-		{describeOptions{appOnly: true}, "cosmos_chronos_describe_app.expected.txt"},
-		{describeOptions{config: true}, "cosmos_chronos_describe_config.expected.json"},
-		{describeOptions{appOnly: true, render: true}, "cosmos_chronos_describe_app_render.expected.json"},
-		{describeOptions{allVersions: true}, "cosmos_chronos_describe_pkg_versions.expected.json"},
+		{describeOptions{}, string(expectedDescribe)},
+		{describeOptions{cliOnly: true}, ""},
+		{describeOptions{appOnly: true}, "{\n    \"id\": \"{{helloworld.id}}\",\n    \"cmd\": \"echo 'Hello World'\"\n}"},
+		{describeOptions{config: true}, "{\n  \"foo\": \"bar\"\n}\n"},
+		{describeOptions{appOnly: true, render: true}, "{\n  \"foo\": \"bar\"\n}\n"},
+		{describeOptions{allVersions: true}, "[\n  \"1\",\n  \"2\",\n  \"3\"\n]\n"},
 	}
 	for _, tt := range testCases {
-		t.Run(tt.out, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%#v", tt.in), func(t *testing.T) {
 			var out bytes.Buffer
 			env.Out = &out
 
-			path := filepath.Join("testdata", tt.out)
-			expected, err := ioutil.ReadFile(path)
-			assert.NoError(t, err)
-
 			err = pkgDescribe(ctx, "helloworld", tt.in, client)
 			assert.NoError(t, err)
-			assert.Equal(t, string(expected), string(out.Bytes()))
+			assert.Equal(t, tt.out, string(out.Bytes()))
 		})
 	}
+}
+
+func Test_pkgDescribe_cli_only(t *testing.T) {
+	var out bytes.Buffer
+	env := mock.NewEnvironment()
+	env.Fs = afero.NewCopyOnWriteFs(
+		afero.NewReadOnlyFs(afero.NewOsFs()),
+		afero.NewMemMapFs(),
+	)
+	env.Out = &out
+	ctx := mock.NewContext(env)
+
+	var testCases = []struct {
+		in  cosmos.Description
+		out string
+	}{
+		{cosmos.Description{Package: dcos.CosmosPackage{Command: dcos.CosmosPackageCommand{Name: "some command"}}},
+			"{\n  \"name\": \"some command\"\n}\n"},
+		{cosmos.Description{Package: dcos.CosmosPackage{Command: dcos.CosmosPackageCommand{Pip: []string{"deprecated"}}}},
+			"{\n  \"pip\": [\n    \"deprecated\"\n  ]\n}\n"},
+		{cosmos.Description{Package: dcos.CosmosPackage{Command: dcos.CosmosPackageCommand{Name: "some command", Pip: []string{"deprecated"}}}},
+			"{\n  \"name\": \"some command\",\n  \"pip\": [\n    \"deprecated\"\n  ]\n}\n"},
+		{cosmos.Description{Package: dcos.CosmosPackage{
+			Command: dcos.CosmosPackageCommand{Name: "some command", Pip: []string{"deprecated"}},
+			Resource: dcos.CosmosPackageResource{Cli: dcos.CosmosPackageResourceCli{Binaries: dcos.CosmosPackageResourceCliBinaries{
+				Linux: dcos.CosmosPackageResourceCliOsBinaries{X8664: dcos.CosmosPackageResourceCliArtifact{
+					Kind: "zip", Url: "http://exmaple.com"}}}}}}},
+			"{\n  \"binaries\": {\n    \"darwin\": {\n      \"x86-64\": {}\n    },\n    \"linux\": {\n      \"x86-64\": {\n        \"kind\": \"zip\",\n        \"url\": \"http://exmaple.com\"\n      }\n    },\n    \"windows\": {\n      \"x86-64\": {}\n    }\n  }\n}\n"},
+		{cosmos.Description{Package: dcos.CosmosPackage{
+			Resource: dcos.CosmosPackageResource{Cli: dcos.CosmosPackageResourceCli{Binaries: dcos.CosmosPackageResourceCliBinaries{
+				Linux: dcos.CosmosPackageResourceCliOsBinaries{X8664: dcos.CosmosPackageResourceCliArtifact{
+					Kind: "zip", Url: "http://exmaple.com"}}}}}}},
+			"{\n  \"binaries\": {\n    \"darwin\": {\n      \"x86-64\": {}\n    },\n    \"linux\": {\n      \"x86-64\": {\n        \"kind\": \"zip\",\n        \"url\": \"http://exmaple.com\"\n      }\n    },\n    \"windows\": {\n      \"x86-64\": {}\n    }\n  }\n}\n"},
+	}
+	for _, tt := range testCases {
+		t.Run(fmt.Sprintf("%#v", tt.in), func(t *testing.T) {
+			client := &mocks.Client{}
+			client.On("PackageDescribe", "helloworld", "").Return(&tt.in, nil)
+
+			var out bytes.Buffer
+			env.Out = &out
+
+			err := pkgDescribe(ctx, "helloworld", describeOptions{cliOnly: true}, client)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.out, string(out.Bytes()))
+		})
+	}
+}
+
+func Test_pkgDescribe_cosmos_render_error(t *testing.T) {
+	var out bytes.Buffer
+	env := mock.NewEnvironment()
+	env.Fs = afero.NewCopyOnWriteFs(
+		afero.NewReadOnlyFs(afero.NewOsFs()),
+		afero.NewMemMapFs(),
+	)
+	env.Out = &out
+	ctx := mock.NewContext(env)
+
+	client := &mocks.Client{}
+	desc := cosmos.Description{}
+	client.On("PackageDescribe", "helloworld", "").Return(&desc, nil)
+	client.On("PackageRender", "", "helloworld", "", "").Return(nil, fmt.Errorf("render error"))
+
+	err := pkgDescribe(ctx, "helloworld", describeOptions{appOnly: true, render: true}, client)
+	assert.EqualError(t, err, "render error")
+	assert.Empty(t, string(out.Bytes()))
+}
+
+func Test_pkgDescribe_cosmos_error(t *testing.T) {
+	var out bytes.Buffer
+	env := mock.NewEnvironment()
+	env.Fs = afero.NewCopyOnWriteFs(
+		afero.NewReadOnlyFs(afero.NewOsFs()),
+		afero.NewMemMapFs(),
+	)
+	env.Out = &out
+	ctx := mock.NewContext(env)
+
+	client := &mocks.Client{}
+	client.On("PackageDescribe", "helloworld", "").Return(nil, fmt.Errorf("could not describe"))
+	client.On("PackageListVersions", "helloworld").Return(nil, fmt.Errorf("could not get version"))
+
+	var testCases = []struct {
+		in  describeOptions
+		out string
+	}{
+		{describeOptions{}, "could not describe"},
+		{describeOptions{cliOnly: true}, "could not describe"},
+		{describeOptions{appOnly: true}, "could not describe"},
+		{describeOptions{config: true}, "could not describe"},
+		{describeOptions{appOnly: true, render: true}, "could not describe"},
+		{describeOptions{allVersions: true}, "could not get version"},
+	}
+	for _, tt := range testCases {
+		t.Run(fmt.Sprintf("%#v", tt.in), func(t *testing.T) {
+			var out bytes.Buffer
+			env.Out = &out
+
+			err := pkgDescribe(ctx, "helloworld", tt.in, client)
+			assert.EqualError(t, err, tt.out)
+			assert.Empty(t, string(out.Bytes()))
+		})
+	}
+}
+
+func Test_pkgDescribe_error_on_decode_base64(t *testing.T) {
+	var out bytes.Buffer
+	env := mock.NewEnvironment()
+	env.Fs = afero.NewCopyOnWriteFs(
+		afero.NewReadOnlyFs(afero.NewOsFs()),
+		afero.NewMemMapFs(),
+	)
+	env.Out = &out
+	ctx := mock.NewContext(env)
+
+	client := &mocks.Client{}
+	desc := cosmos.Description{
+		Package: dcos.CosmosPackage{
+			Marathon: dcos.CosmosPackageMarathon{
+				V2AppMustacheTemplate: "not base 64",
+			},
+		},
+	}
+	client.On("PackageDescribe", "helloworld", "").Return(&desc, nil)
+
+	err := pkgDescribe(ctx, "helloworld", describeOptions{appOnly: true, config: true}, client)
+	assert.EqualError(t, err, "illegal base64 data at input byte 3")
+	assert.Empty(t, string(out.Bytes()))
 }
