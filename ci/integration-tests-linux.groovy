698647d5a657086d4eb7dff26ffef4d11ca66a25
Tomasz Janiszewski <janiszt@gmail.com>
2019-10-30 17:44:51 +0100

    WIP

--- ci/integration-tests-linux.groovy
+++ ci/integration-tests-linux.groovy
@@ -43,8 +43,9 @@ pipeline {
                  credentialsId: '23743034-1ac4-49f7-b2e6-a661aee2d11b',
                  variable     : 'DCOS_TEST_SSH_KEY_PATH']
         ]) {
-          sh "ssh-keygen -y -f ${DCOS_TEST_SSH_KEY_PATH} > ./id_rsa.pub"
-          sh 'ssh-agent sh -c "ssh-add ${DCOS_TEST_SSH_KEY_PATH} && ./terraform init scripts && ./terraform apply -auto-approve scripts"'
+          sh "./terraform init scripts"
+          sh "ssh-keygen -t rsa -f id_rsa"
+          sh 'ssh-agent sh -c "ssh-add ./id_rsa && ./terraform apply -auto-approve scripts"'
           script {
             MASTER_PUBLIC_IP = sh(
                     script: './terraform output --json -module dcos.dcos-infrastructure masters.public_ips | ./jq -r \'.value[0]\'',
