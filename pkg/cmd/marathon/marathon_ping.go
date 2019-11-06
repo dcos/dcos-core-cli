3936292e76dd674f888d35a81070ee852becbe54
Armand Grillet <armand.grillet@outlook.com>
2019-11-06 14:51:56 +0100

    Port 'dcos marathon ping' in Go.
    Adresses https://jira.mesosphere.com/browse/DCOS-60888

--- pkg/cmd/marathon/marathon_ping.go
+++ pkg/cmd/marathon/marathon_ping.go
@@ -1,8 +1,12 @@
 package marathon
 
 import (
+	"fmt"
+
 	"github.com/dcos/dcos-cli/api"
-	"github.com/dcos/dcos-core-cli/pkg/cmd/marathon/python"
+	"github.com/dcos/dcos-core-cli/pkg/marathon"
+	"github.com/dcos/dcos-core-cli/pkg/mesos"
+	"github.com/dcos/dcos-core-cli/pkg/pluginutil"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +17,19 @@ func newCmdMarathonPing(ctx api.Context) *cobra.Command {
 		Use:   "ping",
 		Short: "Ensure Marathon is up and responding.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return python.InvokePythonCLI(ctx)
+			client, err := marathon.NewClient(ctx)
+			if err != nil {
+				return err
+			}
+
+			masters, err := marathonPing(ctx, *client, once)
+			if err != nil {
+				return err
+			}
+
+			// If no err, a pong has been received.
+			fmt.Fprintf(ctx.Out(), "Marathon ping response[%dx]: \"pong\"\n", masters)
+			return nil
 		},
 	}
 
@@ -21,4 +37,28 @@ func newCmdMarathonPing(ctx api.Context) *cobra.Command {
 	cmd.Flags().BoolVar(&once, "once", false, "")
 
 	return cmd
+}
+
+func marathonPing(ctx api.Context, client marathon.Client, once bool) (int, error) {
+	numberOfMasters := 1
+	if !once {
+		cluster, err := ctx.Cluster()
+		if err != nil {
+			return 0, err
+		}
+		mesosClient := mesos.NewClient(pluginutil.HTTPClient(cluster.URL()))
+		masters, err := mesosClient.Masters()
+		if err != nil {
+			return 0, err
+		}
+		numberOfMasters = len(masters)
+	}
+
+	for i := 1; i <= numberOfMasters; i++ {
+		_, err := client.API.Ping()
+		if err != nil {
+			return 0, fmt.Errorf("unable to ping leading Marathon master %d time(s)", i)
+		}
+	}
+	return numberOfMasters, nil
 }
