8f5f4021cb55531e2045119595f5a871e03ea8a3
janisz <janiszt@gmail.com>
2019-11-06 22:35:00 +0100

    Remove version check

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -8,7 +8,6 @@ cd python/lib/dcoscli
 source env/bin/activate
 wget -qO env/bin/dcos https://downloads.dcos.io/cli/testing/binaries/dcos/${OS}/x86-64/master/dcos
 chmod +x env/bin/dcos
-dcos --version
 dcos cluster remove --all
 dcos cluster setup --no-check ${DCOS_TEST_URL}
 dcos plugin add -u ../../../build/$OS/dcos-core-cli.zip
