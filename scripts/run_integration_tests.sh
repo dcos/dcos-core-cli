3cc742dfdd2caec3e4a36c605d47c2b5bd848f76
janisz <janiszt@gmail.com>
2019-11-08 16:00:43 +0100

    Update run_integration_tests.sh

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -12,7 +12,6 @@ cd python/lib/dcoscli
 source env/bin/activate
 wget -qO env/bin/dcos https://downloads.dcos.io/cli/testing/binaries/dcos/${OS}/x86-64/master/dcos
 chmod +x env/bin/dcos
-dcos cluster remove --all
 dcos cluster setup --no-check ${DCOS_TEST_URL}
 dcos plugin add -u ../../../build/$OS/dcos-core-cli.zip
 py.test -vv -x --durations=10 -p no:cacheprovider tests/integrations
