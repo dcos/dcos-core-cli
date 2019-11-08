ed1973369a3c6f44ec2e51bf2c100870a9200732
Brian Lewis <keller@csh.rit.edu>
2019-11-08 15:22:31 +0100

    Fix linting issues

--- cmd/dcos/main.go
+++ cmd/dcos/main.go
@@ -11,8 +11,11 @@ import (
 	"github.com/dcos/dcos-core-cli/pkg/cmd"
 )
 
-const invalidCertError = `An SSL error occurred. To configure your SSL settings, please run: 'dcos config set core.ssl_verify <value>'
-<value>: Whether to verify SSL certs for HTTPS or path to certs.  Valid values are a path to a CA_BUNDLE, True (will then use CA Certificates from certifi), or False (will then send insecure requests).\n`
+const invalidCertError = `An SSL error occurred. To configure your SSL settings, " +
+"please run: 'dcos config set core.ssl_verify <value>\n'
+<value>: Whether to verify SSL certs for HTTPS or path to certs. " +
+"Valid values are a path to a CA_BUNDLE, True (will then use CA " +
+"Certificates from certifi), or False (will then send insecure requests).\n`
 
 func main() {
 	ctx := cli.NewContext(cli.NewOsEnvironment())
