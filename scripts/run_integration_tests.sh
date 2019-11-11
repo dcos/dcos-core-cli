e8a5c968496274686d3fd85c47cda5f23606cf6d
janisz <janiszt@gmail.com>
2019-11-11 18:57:07 +0100

    Create random dir (#400)
    * Use python 3.7
    
    * Use random directory to allow concurrent builds.
    
    We have a lots of failures due to unknonw flag. This might be result of
    concurrent builds on mac that intererer withe each other. To prevent
    this we should use temp directory for dcos instead of default ~/.dcos
    
    * Update run_integration_tests.sh

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -3,6 +3,7 @@ set -x
 export PYTHONIOENCODING=utf-8
 export CLI_TEST_SSH_USER=centos
 export CLI_TEST_MASTER_PROXY=1
+export DCOS_DIR=$(mktemp)
 export PYTHON=python3.7
 export LANG=en_US.utf-8
 export LC_ALL=en_US.utf-8
@@ -11,7 +12,7 @@ cd python/lib/dcoscli
 source env/bin/activate
 wget -qO env/bin/dcos https://downloads.dcos.io/cli/testing/binaries/dcos/${OS}/x86-64/master/dcos
 chmod +x env/bin/dcos
-dcos cluster remove --all
 dcos cluster setup --no-check ${DCOS_TEST_URL}
 dcos plugin add -u ../../../build/$OS/dcos-core-cli.zip
 py.test -vv -x --durations=10 -p no:cacheprovider tests/integrations
+rm -rf $DCOS_DIR
