1d0a27c176582eb7b6e6a2014a928337cf67428a
Brian Lewis <keller@csh.rit.edu>
2019-11-22 15:32:08 +0100

    Change marathon app stop into Go

--- pkg/cmd/marathon/app/app_stop.go
+++ pkg/cmd/marathon/app/app_stop.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/dcos/dcos-cli/api"
-	"github.com/dcos/dcos-core-cli/pkg/cmd/marathon/python"
+	"github.com/dcos/dcos-core-cli/pkg/marathon"
+	goMarathon "github.com/gambol99/go-marathon"
 	"github.com/spf13/cobra"
 )
 
@@ -11,13 +14,44 @@ func newCmdMarathonAppStop(ctx api.Context) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:   "stop",
+		Args:  cobra.ExactArgs(1),
 		Short: "Stop an application.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return python.InvokePythonCLI(ctx)
+			appID := args[0]
+
+			return appStop(ctx, appID, force)
 		},
 	}
 
 	cmd.Flags().BoolVar(&force, "force", false, "Disable checks in Marathon during updates.")
 
 	return cmd
+}
+
+func appStop(ctx api.Context, appID string, force bool) error {
+	client, err := marathon.NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	description, err := client.API.Application(appID)
+	if err != nil {
+		if apiErr, ok := err.(*goMarathon.APIError); ok && apiErr.ErrCode == goMarathon.ErrCodeNotFound {
+			return fmt.Errorf(`app '%s' does not exist`, appID)
+		}
+		return err
+	}
+
+	fmt.Println(description.Instances)
+	if *description.Instances <= 0 {
+		return fmt.Errorf("application '%s' already stopped: %d instances", appID, *description.Instances)
+	}
+
+	deploymentID, err := client.API.ScaleApplicationInstances(appID, 0, force)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(ctx.Out(), "Created deployment %s\n", deploymentID.DeploymentID)
+
+	return nil
 }
---
+++ pkg/cmd/marathon/app/app_stop_test.go
@@ -0,0 +1,165 @@
+package app
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/dcos/dcos-cli/pkg/config"
+	"github.com/dcos/dcos-cli/pkg/mock"
+	goMarathon "github.com/gambol99/go-marathon"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAppStop(t *testing.T) {
+	app := struct {
+		App goMarathon.Application `json:"app"`
+	}{
+		App: goMarathon.Application{
+			ID:        "test",
+			Instances: intPointer(3),
+		},
+	}
+	deployment := goMarathon.DeploymentID{
+		DeploymentID: "5ed4c0c5-9ff8-4a6f-a0cd-f57f59a34b43",
+		Version:      "2015-09-29T15:59:51.164Z",
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch url := r.URL.String(); url {
+		//nolint:goconst
+		case "/service/marathon/v2/apps/test":
+			switch r.Method {
+			case http.MethodGet:
+				w.WriteHeader(200)
+				json.NewEncoder(w).Encode(app)
+			case http.MethodPut:
+				w.WriteHeader(200)
+				json.NewEncoder(w).Encode(deployment)
+			default:
+				require.Fail(t, "unexpected http verb", r.Method)
+			}
+		default:
+			require.Fail(t, "unexpected call to endpoint", url)
+		}
+	}))
+
+	out := new(bytes.Buffer)
+	env := mock.NewEnvironment()
+	env.Out = out
+	ctx := mock.NewContext(env)
+	cluster := config.NewCluster(nil)
+	cluster.SetURL(ts.URL)
+	ctx.SetCluster(cluster)
+
+	err := appStop(ctx, "/test", false)
+	require.NoError(t, err)
+
+	assert.Equal(t, "Created deployment 5ed4c0c5-9ff8-4a6f-a0cd-f57f59a34b43\n", out.String())
+
+}
+
+func TestAppStopWhenAppIsStopped(t *testing.T) {
+	app := struct {
+		App goMarathon.Application `json:"app"`
+	}{
+		App: goMarathon.Application{
+			ID:        "test",
+			Instances: intPointer(0),
+		},
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch url := r.URL.String(); url {
+		case "/service/marathon/v2/apps/test":
+			switch r.Method {
+			case http.MethodGet:
+				w.WriteHeader(200)
+				json.NewEncoder(w).Encode(app)
+			default:
+				require.Fail(t, "unexpected http verb", url, r.Method)
+			}
+		default:
+			require.Fail(t, "unexpected call to endpoint", url)
+		}
+	}))
+
+	out := new(bytes.Buffer)
+	env := mock.NewEnvironment()
+	env.Out = out
+	ctx := mock.NewContext(env)
+	cluster := config.NewCluster(nil)
+	cluster.SetURL(ts.URL)
+	ctx.SetCluster(cluster)
+
+	err := appStop(ctx, "/test", false)
+	assert.EqualError(t, err, "application '/test' already stopped: 0 instances")
+}
+
+func TestAppStopWhenAppDoesntExist(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch url := r.URL.String(); url {
+		case "/service/marathon/v2/apps/test":
+			w.WriteHeader(http.StatusNotFound)
+		default:
+			require.Fail(t, "unexpected call to endpoint", url)
+		}
+	}))
+
+	out := new(bytes.Buffer)
+	env := mock.NewEnvironment()
+	env.Out = out
+	ctx := mock.NewContext(env)
+	cluster := config.NewCluster(nil)
+	cluster.SetURL(ts.URL)
+	ctx.SetCluster(cluster)
+	err := appStop(ctx, "/test", false)
+	assert.EqualError(t, err, "app '/test' does not exist")
+}
+
+func TestAppStopForce(t *testing.T) {
+	app := struct {
+		App goMarathon.Application `json:"app"`
+	}{
+		App: goMarathon.Application{
+			ID:        "test",
+			Instances: intPointer(2),
+		},
+	}
+	deployment := goMarathon.DeploymentID{
+		DeploymentID: "5ed4c0c5-9ff8-4a6f-a0cd-f57f59a34b43",
+		Version:      "2015-09-29T15:59:51.164Z",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch url := r.URL.Path; url {
+		case "/service/marathon/v2/apps/test":
+			switch r.Method {
+			case http.MethodGet:
+				w.WriteHeader(200)
+				json.NewEncoder(w).Encode(app)
+			case http.MethodPut:
+				assert.Contains(t, r.URL.Query(), "force")
+				w.WriteHeader(200)
+				json.NewEncoder(w).Encode(deployment)
+			default:
+				require.Fail(t, "unexpected http verb", url, r.Method)
+			}
+		default:
+			require.Fail(t, "unexpected call to endpoint", url)
+		}
+	}))
+
+	out := new(bytes.Buffer)
+	env := mock.NewEnvironment()
+	env.Out = out
+	ctx := mock.NewContext(env)
+	cluster := config.NewCluster(nil)
+	cluster.SetURL(ts.URL)
+	ctx.SetCluster(cluster)
+
+	err := appStop(ctx, "/test", true)
+	assert.NoError(t, err)
+}
