45d0455bf85412bd3b793b198ff2151e31e0f2df
Brian Lewis <keller@csh.rit.edu>
2019-11-25 12:19:58 +0100

    Forgot to remove a period...

--- python/lib/dcoscli/tests/integrations/test_marathon.py
+++ python/lib/dcoscli/tests/integrations/test_marathon.py
@@ -226,7 +226,7 @@ def test_stop_app():
 def test_stop_already_stopped_app():
     with _zero_instance_app():
         stderr = (b"Error: app '/zero-instance-app' already "
-                  b"stopped: 0 instances.\n")
+                  b"stopped: 0 instances\n")
         assert_command(
             ['dcos', 'marathon', 'app', 'stop', 'zero-instance-app'],
             returncode=1,
