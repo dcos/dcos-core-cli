8cf7abbdb1264d904bacd836e88a0e94ca741b9f
Brian Lewis <keller@csh.rit.edu>
2019-12-03 13:47:39 +0100

    remove redundant code

--- pkg/cmd/marathon/debug/debug_list_test.go
+++ pkg/cmd/marathon/debug/debug_list_test.go
@@ -1,14 +1,11 @@
 package debug
 
 import (
-	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/dcos/dcos-cli/pkg/config"
-	"github.com/dcos/dcos-cli/pkg/mock"
 	goMarathon "github.com/gambol99/go-marathon"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -68,17 +65,6 @@ func TestDebugList(t *testing.T) {
 		"  /test-pod  2019-11-28T14:31:09.156Z                    2  false                 100          " +
 		"   90  2019-11-28T14:25:07.631Z  2019-11-28T14:24:07.631Z  \n"
 	assert.Equal(t, expected, out.String())
-}
-
-func newContext(ts *httptest.Server) (*mock.Context, *bytes.Buffer) {
-	out := new(bytes.Buffer)
-	env := mock.NewEnvironment()
-	env.Out = out
-	ctx := mock.NewContext(env)
-	cluster := config.NewCluster(nil)
-	cluster.SetURL(ts.URL)
-	ctx.SetCluster(cluster)
-	return ctx, out
 }
 
 func strPointer(s string) *string {
