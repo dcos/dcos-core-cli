de7d37d0c10af70dfbf86effcba4e74bd3392b0a
Tomasz Janiszewski <janiszt@gmail.com>
2020-04-15 13:15:27 +0200

    script

--- ci/integration-tests-linux.groovy
+++ ci/integration-tests-linux.groovy
@@ -42,25 +42,7 @@ pipeline {
             steps {
                 withCredentials(credentials) {
                     script {
-                        sh(script: '''
-                                  cd scripts && \
-                                  export AWS_REGION="us-east-1" && \
-                                  export TF_VAR_dcos_user=$DCOS_USERNAME  && \
-                                  export TF_VAR_dcos_pass_hash=$(perl -e 'print crypt($ENV{DCOS_PASSWORD},"\\$6\\$1234567890\\$")') && \
-                                  export TF_VAR_dcos_license_key_contents=$DCOS_TEST_LICENSE && \
-                                  export TF_VAR_custom_dcos_download_path=$DCOS_TEST_INSTALLER_URL && \
-                                  export CLI_TEST_SSH_KEY_PATH && \
-                                  export TF_INPUT=false && \
-                                  export TF_IN_AUTOMATION=1 && \
-                                  wget -nv https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip && \
-                                  unzip -o terraform_0.11.14_linux_amd64.zip && \
-                                  mkdir -p $HOME/.ssh
-                                  eval $(ssh-agent) && \
-                                  ssh-add $CLI_TEST_SSH_KEY_PATH && \
-                                  ssh-keygen -y -f $CLI_TEST_SSH_KEY_PATH > $HOME/.ssh/id_rsa.pub && \
-                                  ./terraform init -no-color && \
-                                  ./terraform  apply -auto-approve -no-color''')
-                        master_ip = sh(script: 'cd scripts && ./terraform output masters_public_ip', returnStdout: true).trim()
+                        master_ip = sh(script: 'cd scripts && ./launch_aws_cluster.sh', returnStdout: true).trim()
                     }
                     stash includes: 'scripts', name: 'terraform'
                 }
