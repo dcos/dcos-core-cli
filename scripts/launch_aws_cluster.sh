432b5271c7a6393bc578a293120bf11da5e767a4
Tomasz Janiszewski <janiszt@gmail.com>
2020-04-15 14:33:43 +0200

    redirect

--- scripts/launch_aws_cluster.sh
+++ scripts/launch_aws_cluster.sh
@@ -11,9 +11,9 @@ export TF_IN_AUTOMATION=1
 wget -q https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
 unzip -qq -o terraform_0.11.14_linux_amd64.zip
 mkdir -p $HOME/.ssh
-eval $(ssh-agent)
-ssh-add $CLI_TEST_SSH_KEY_PATH
+eval $(ssh-agent) >&2
+ssh-add $CLI_TEST_SSH_KEY_PATH >&2
 ssh-keygen -y -f $CLI_TEST_SSH_KEY_PATH > $HOME/.ssh/id_rsa.pub
-./terraform init -no-color
-./terraform  apply -auto-approve -no-color
+./terraform init -no-color >&2
+./terraform  apply -auto-approve -no-color >&2
 ./terraform output masters_public_ip
