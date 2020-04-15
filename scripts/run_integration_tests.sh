991daccf4032ad7c8a0266b0b600cbe0f0c51c9f
Tomasz Janiszewski <janiszt@gmail.com>
2020-04-15 22:46:39 +0200

    ds

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -21,6 +21,7 @@ rm -f env/bin/dcos
 wget -qO env/bin/dcos https://downloads.dcos.io/cli/testing/binaries/dcos/${OS}/x86-64/master/dcos
 chmod +x env/bin/dcos
 dcos --version
+wget ${DCOS_TEST_URL}
 dcos cluster setup --no-check ${DCOS_TEST_URL}
 dcos plugin add -u ../../../build/$OS/dcos-core-cli.zip
 
