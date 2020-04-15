76e6d4c71e59923f47516b9cdfdc5a8cb7541bb9
Tomasz Janiszewski <janiszt@gmail.com>
2020-04-15 12:03:03 +0200

    escape

--- ci/integration-tests-linux.groovy
+++ ci/integration-tests-linux.groovy
@@ -46,7 +46,7 @@ pipeline {
                                   cd scripts && \
                                   export AWS_REGION="us-east-1" && \
                                   export TF_VAR_dcos_user=$DCOS_USERNAME  && \
-                                  export TF_VAR_dcos_pass_hash=$(perl -e 'print crypt($ENV{DCOS_PASSWORD},"$6$1234567890$")') && \
+                                  export TF_VAR_dcos_pass_hash=$(perl -e 'print crypt($ENV{DCOS_PASSWORD},"\\$6\\$1234567890\\$")') && \
                                   export TF_VAR_dcos_license_key_contents=$DCOS_TEST_LICENSE && \
                                   export TF_VAR_custom_dcos_download_path=$DCOS_TEST_INSTALLER_URL && \
                                   export CLI_TEST_SSH_KEY_PATH && \
