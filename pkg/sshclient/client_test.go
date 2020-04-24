2e68bd45d924c9b6f71b8f6bfe1d626404381f1d
Tomasz Janiszewski <janiszt@gmail.com>
2020-04-24 15:34:10 +0200

    SSH accept --user flag without proxy.
    Fixes: COPS-5977 D2IQ-66924

--- pkg/sshclient/client_test.go
+++ pkg/sshclient/client_test.go
@@ -2,12 +2,13 @@ package sshclient
 
 import (
 	"bytes"
-	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"os"
 	"strings"
 	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewClientArgs(t *testing.T) {
@@ -20,14 +21,21 @@ func TestNewClientArgs(t *testing.T) {
 	}{
 		{name: "no opts", given: ClientOpts{}, expected: []string{"-A", "-t", ""}},
 		{name: "given host", given: ClientOpts{Host: "192.0.2.1"}, expected: []string{"-A", "-t", "192.0.2.1"}},
-		{name: "proxy", given: ClientOpts{Proxy: "192.0.2.1"}, expected: []string{"-A", "-t", "-l", "", "192.0.2.1", "ssh", "-l", "", "-A", "-t", ""}},
+		{name: "proxy", given: ClientOpts{Proxy: "192.0.2.1"},
+			expected: []string{"-A", "-t", "-l", "", "192.0.2.1", "ssh", "-l", "", "-A", "-t", ""}},
 		{name: "config", given: ClientOpts{Config: "ssh.config"}, expected: []string{"-F", "ssh.config", "-A", "-t", ""}},
-		{name: "user", given: ClientOpts{User: "root"}, expected: []string{"-A", "-t", ""}},
-		{name: "shh options", given: ClientOpts{SSHOptions: []string{"-6", "-C", "-q"}}, expected: []string{"-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", ""}},
-		{name: "user and proxy", given: ClientOpts{User: "root", Proxy: "192.0.2.1"}, expected: []string{"-A", "-t", "-l", "root", "192.0.2.1", "ssh", "-l", "root", "-A", "-t", ""}},
-		{name: "config and proxy", given: ClientOpts{Config: "ssh.config", Proxy: "192.0.2.1"}, expected: []string{"-F", "ssh.config", "-A", "-t", "192.0.2.1", "ssh", "-A", "-t", ""}},
-		{name: "user and config and proxy", given: ClientOpts{User: "root", Config: "ssh.config", Proxy: "192.0.2.1"}, expected: []string{"-F", "ssh.config", "-A", "-t", "192.0.2.1", "ssh", "-A", "-t", ""}},
-		{name: "options and proxy", given: ClientOpts{SSHOptions: []string{"-6", "-C", "-q"}, Proxy: "192.0.2.1"}, expected: []string{"-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", "-l", "", "192.0.2.1", "ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-l", "", "-A", "-t", ""}},
+		{name: "user", given: ClientOpts{User: "root"}, expected: []string{"-l", "root", "-A", "-t", ""}},
+		{name: "shh options", given: ClientOpts{SSHOptions: []string{"-6", "-C", "-q"}},
+			expected: []string{"-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", ""}},
+		{name: "user and proxy", given: ClientOpts{User: "root", Proxy: "192.0.2.1"},
+			expected: []string{"-A", "-t", "-l", "root", "192.0.2.1", "ssh", "-l", "root", "-A", "-t", ""}},
+		{name: "config and proxy", given: ClientOpts{Config: "ssh.config", Proxy: "192.0.2.1"},
+			expected: []string{"-F", "ssh.config", "-A", "-t", "192.0.2.1", "ssh", "-A", "-t", ""}},
+		{name: "user and config and proxy", given: ClientOpts{User: "root", Config: "ssh.config", Proxy: "192.0.2.1"},
+			expected: []string{"-F", "ssh.config", "-A", "-t", "192.0.2.1", "ssh", "-A", "-t", ""}},
+		{name: "options and proxy", given: ClientOpts{SSHOptions: []string{"-6", "-C", "-q"}, Proxy: "192.0.2.1"},
+			expected: []string{"-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", "-l", "", "192.0.2.1",
+				"ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-l", "", "-A", "-t", ""}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -67,7 +75,7 @@ func TestNewClientSetDefault(t *testing.T) {
 			ErrOut:     os.Stderr,
 		},
 	}, client)
-	assert.Contains(t,  buf.String(), "Trying to establish connection to")
+	assert.Contains(t, buf.String(), "Trying to establish connection to")
 }
 
 func TestNewClientDoNotOverrideInputOutAndErrorOutWhenSet(t *testing.T) {
@@ -98,21 +106,24 @@ func TestPrepareCommandEmptyCommand(t *testing.T) {
 	logger.SetOutput(&buf)
 	logger.SetLevel(logrus.DebugLevel)
 	opts := ClientOpts{
-		Config: "ssh.config",
-		Host: "192.0.2.2",
-		Proxy: "192.0.2.1",
+		Config:     "ssh.config",
+		Host:       "192.0.2.2",
+		Proxy:      "192.0.2.1",
 		SSHOptions: []string{"-6", "-C", "-q"},
-		User: "root",
+		User:       "root",
 	}
 	c, err := NewClient(opts, logger)
 	require.NoError(t, err)
 
 	cmd := c.prepareCommand(nil)
-	assert.Equal(t, []string{"/usr/bin/ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-F", "ssh.config", "-A", "-t", "192.0.2.1", "ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", "192.0.2.2"}, cmd.Args)
+	assert.Equal(t, []string{"/usr/bin/ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-F", "ssh.config", "-A", "-t", "192.0.2.1",
+		"ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", "192.0.2.2"}, cmd.Args)
 	cmd = c.prepareCommand([]string{"/bin/bash", "'-c'", "echo \"OK\""})
-	assert.Equal(t, []string{"/usr/bin/ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-F", "ssh.config", "-A", "-t", "192.0.2.1", "ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", "192.0.2.2", "'", "/bin/bash", "'-c'", "echo \"OK\"", "'"}, cmd.Args)
+	assert.Equal(t, []string{"/usr/bin/ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-F", "ssh.config", "-A", "-t", "192.0.2.1",
+		"ssh", "-o", "-6", "-o", "-C", "-o", "-q", "-A", "-t", "192.0.2.2", "'", "/bin/bash", "'-c'", "echo \"OK\"", "'"}, cmd.Args)
 	assert.Equal(t, os.Stdin, cmd.Stdin)
 	assert.Equal(t, os.Stdout, cmd.Stdout)
 	assert.Equal(t, os.Stderr, cmd.Stderr)
-	assert.Contains(t, buf.String(), "Running: [/usr/bin/ssh -o -6 -o -C -o -q -F ssh.config -A -t 192.0.2.1 ssh -o -6 -o -C -o -q -A -t 192.0.2.2]")
+	assert.Contains(t, buf.String(), "Running: [/usr/bin/ssh -o -6 -o -C -o -q -F ssh.config -A -t 192.0.2.1 "+
+		"ssh -o -6 -o -C -o -q -A -t 192.0.2.2]")
 }
