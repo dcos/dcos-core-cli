7a4f33c484673816b1e3c676c167b1644e92cb7e
janisz <janiszt@gmail.com>
2020-06-01 19:31:11 +0200

    Update dcos node diagnostic deprecation message
    Move deprecation message to higher level to avoid duplication.
    Use `ctx.Out()` instead of just `fmt.Printf` to be consistend with the
    rest of the code.
    Remove ctx when not used.
    
    Fixes: COPS-6209

--- pkg/cmd/node/node_diagnostics_create.go
+++ pkg/cmd/node/node_diagnostics_create.go
@@ -13,16 +13,11 @@ func newCmdNodeDiagnosticsCreate(ctx api.Context) *cobra.Command {
 		Short: "Create a diagnostics bundle",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			err := ctx.Deprecated("This command is deprecated since DC/OS 2.0, please use 'dcos diagnostics create' instead.")
-			if err != nil {
-				return err
-			}
-
 			resp, err := diagnosticsClient().Create(args)
 			if err != nil {
 				return err
 			}
-			fmt.Printf("%s, available bundle: %s\n", resp.Status, resp.Extra.BundleName)
+			fmt.Fprintf(ctx.Out(), "%s, available bundle: %s\n", resp.Status, resp.Extra.BundleName)
 			return nil
 		},
 	}
