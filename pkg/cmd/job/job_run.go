16c7cd8c0f37114a1aa681d80a502475942d25e2
Tomasz Janiszewski <janiszt@gmail.com>
2020-06-02 13:21:26 +0200

    Add JSON output to job run command
    Add missing --json flag to job run command.
    This function was in Python code but was missing in Go.
    
    Fixes: D2IQ-68975

--- pkg/cmd/job/job_run.go
+++ pkg/cmd/job/job_run.go
@@ -1,6 +1,7 @@
 package job
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/dcos/dcos-cli/api"
@@ -9,6 +10,7 @@ import (
 
 // newCmdClusterRun runs a given job right now.
 func newCmdJobRun(ctx api.Context) *cobra.Command {
+	var jsonOutput bool
 	cmd := &cobra.Command{
 		Use:   "run <job-id>",
 		Short: "Run a job now",
@@ -23,9 +25,15 @@ func newCmdJobRun(ctx api.Context) *cobra.Command {
 			if err != nil {
 				return err
 			}
+			if jsonOutput {
+				enc := json.NewEncoder(ctx.Out())
+				enc.SetIndent("", "    ")
+				return enc.Encode(run)
+			}
 			fmt.Fprintln(ctx.Out(), run.ID)
 			return nil
 		},
 	}
+	cmd.Flags().BoolVar(&jsonOutput, "json", false, "Print in json format")
 	return cmd
 }
