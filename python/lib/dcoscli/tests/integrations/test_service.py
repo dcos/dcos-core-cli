7250fc4eb9f785f34abbf4cfed547e2b40c5bd9d
janisz <tjaniszewski@d2iq.com>
2020-09-24 16:17:31 +0200

    unskip test_service_inactive_and_completed
    Fixes: https://jira.d2iq.com/browse/D2IQ-67130
    Refs: #430

--- python/lib/dcoscli/tests/integrations/test_service.py
+++ python/lib/dcoscli/tests/integrations/test_service.py
@@ -35,7 +35,6 @@ def test_service_table():
     assert_lines(['dcos', 'service', 'list'], 3)
 
 
-@pytest.mark.skip(reason="D2IQ-67130")
 def test_service_inactive_and_completed():
     package_install('kafka', True, ['--app'])
     wait_for_service('kafka')
